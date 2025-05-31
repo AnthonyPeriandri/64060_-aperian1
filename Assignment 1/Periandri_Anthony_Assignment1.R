b528467fc2fc5c9a7753aabe30582850c92250fe
AnthonyPeriandri <aperian1@kent.edu>
2025-05-30 22:49:08 -0400

    Assignment 1 Submittal

---
+++ Assignment 1/Periandri_Anthony_Assignment1.R
@@ -0,0 +1,20 @@
+read.csv("automobile.csv")
+df<-automobile
+summary(df[,c("wheel.base","length")])
+table(df$engine.location)
+table(df$fuel.type)
+df<-df%>%
+  mutate(log_Lengthofcar= log(length))
+summary(df[,c("log_Lengthofcar")])
+hist(df$length,
+     main = "Histogram of Car Length",
+     xlab = "length",
+     col = "red",
+     breaks = 10)
+plot(df$wheel.base,df$length,
+     main = "Wheel Base VS Length",
+     xlab = "Wheel Base",
+     ylab = "Length",
+     col = "blue",
+     pch = 12)
+     
