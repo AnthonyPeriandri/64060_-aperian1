eff2748ff30ed8b8a7342a1849e4febd9570f97c
AnthonyPeriandri <aperian1@kent.edu>
2025-05-30 22:54:30 -0400

    Update Periandri_Anthony_Assignment1.R

--- Assignment 1/Periandri_Anthony_Assignment1.R
+++ Assignment 1/Periandri_Anthony_Assignment1.R
@@ -3,9 +3,9 @@ df<-automobile
 summary(df[,c("wheel.base","length")])
 table(df$engine.location)
 table(df$fuel.type)
-df<-df%>%
+df<-df %>%
   mutate(log_Lengthofcar= log(length))
-summary(df[,c("log_Lengthofcar")])
+summary(df[,c("log_Lengthofcar", "length")])
 hist(df$length,
      main = "Histogram of Car Length",
      xlab = "length",
